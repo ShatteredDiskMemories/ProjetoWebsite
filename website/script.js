205445aabf3f26dca230e07ef48633b57afaa8bb
lucas <141962264+ShatteredDiskMemories@users.noreply.github.com>
2023-09-27 18:13:50 -0300

    scrpt add

---
+++ website/script.js
@@ -0,0 +1,13 @@
+function displayTime() {
+    var dateTime = new Date();
+    var hrs = dateTime.getHours();
+    var min = dateTime.getMinutes();
+    var sec = dateTime.getSeconds();
+
+    document.getElementById('hours').innerHTML = hrs;
+    document.getElementById('minutes').innerHTML = min;
+    document.getElementById('seconds').innerHTML = sec;
+
+}
+
+setInterval(displayTime, 10);
